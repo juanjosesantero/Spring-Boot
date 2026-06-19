ba73f47aa81f4fe7f1ff2e4633fa40d9018c6022
Juan Santero <id00911089@usal.es>
2026-06-19 17:35:41 +0200

    6

---
+++ primero/src/main/java/com/omeg/primero/PrimeroApplication.java
@@ -0,0 +1,13 @@
+package com.omeg.primero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class PrimeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeroApplication.class, args);
+	}
+}
---
+++ teoria.md
@@ -0,0 +1,11 @@
+# Controlador
+Clase que maneja request de los usuarios
+
+Se tiene que poner la anotacion @controller
+
+## Pasar datos a la vista
+Enviar informacion del controlador a una pagina
+
+Patron injeccion de dependecias
+# Configuracion
+En **src\main\resources\application.properties**
